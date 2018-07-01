b81cdfcdfc1985742ea30024540d7661261c3c78
league <league@wts10.attlocal.net>
2018-07-01 13:00:12 -0700

    dsa

---
+++ src/Duc.java
@@ -0,0 +1,12 @@
+
+
+public class Duc {
+	String favoriteFood = "food";
+	int numberOfFriends = 59802374;
+	void Duck(String favoriteFood, int numberOfFriends) {
+      	this.favoriteFood = favoriteFood;
+      	this.numberOfFriends = numberOfFriends;
+}
+
+
+}
