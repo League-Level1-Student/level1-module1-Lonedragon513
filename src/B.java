02dd9767e277e73455afc176436f2c6e82412e6d
DylanArnold101 <league@wts10.attlocal.net>
2018-08-12 12:57:48 -0700

    d

--- src/B.java
+++ src/B.java
@@ -1,19 +1,68 @@
-import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class B implements MouseListener {
-	public static void main(String[] args) {
+public class B implements ActionListener {
+	
 		JFrame f = new JFrame();
 		JPanel p = new JPanel();
 		JButton b = new JButton();
-		JTextField t = new JTextField();
+		JTextField t = new JTextField(20);
 		JLabel l = new JLabel();
-
+	public static void main(String[] args) {
+		new B ().m();
+		
+	
 	}
+		
+	private void m () {
+		f.add(p);
+		f.setVisible(true);
+		f.setTitle("Convert 8 bits of binary to ASCII");
+		b.addActionListener(this);
+		b.setText("convert");
+		p.add(b);
+		p.add(t);
+		p.add(l);
+		f.pack();
+		
+	}
+	 String convert(String input) {
+         if(input.length() != 8){
+              JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+              return "-";
+         }
+         String binary = "[0-1]+";    //must contain numbers in the given range
+         if (!input.matches(binary)) {
+              JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+              return "-";
+         }
+         try {
+              int asciiValue = Integer.parseInt(input, 2);
+              char theLetter = (char) asciiValue;
+              return "" + theLetter;
+         } catch (Exception e) {
+              JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+              return "-";
+         }
+    }
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String jo = t.getText();
+		String joo = convert(jo);
+		l.setText(joo);
+		f.pack();
+	}
+	
+	
 }
